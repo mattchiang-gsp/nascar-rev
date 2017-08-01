5c4e74397ed544686b0ca1a7dcdc91418d5f8e55
Matt <matthew_chiang@gspsf.com>
2017-08-01 10:34:24 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// MP3 players
+var revUpPlayer = new Tone.Player("./rev-up.mp3").toMaster()
+var revDownPlayer = new Tone.Player("./rev-down.mp3").toMaster()
+
+// Timing
+var ctx = new Tone.Context()
+var elapsedTimeSinceUp
+var elapsedTimeSinceDown
+
+// Remember keys pressed
+var keysdown = {}
+
+// On pedal down
+$("body").keydown(function(e) {
+
+	// Is key/pedal already down?
+	if (keysdown[e.keyCode]) {
+		return
+	}
+
+	// Remember the pedal is pressed
+	keysdown[e.keyCode] = true
+
+
+	if (e.keyCode == 66) { // 66 = 'b'
+		// stop rev down sounds
+		elapsedTimeSinceDown = ctx.now() - elapsedTimeSinceDown
+		console.log("Pedal was up for: " + elapsedTimeSinceDown)
+		revDownPlayer.stop()
+
+		// start rev up sounds at 0 or where we left off
+		if (elapsedTimeSinceDown > 0 && elapsedTimeSinceDown < 11) {
+			revUpPlayer.start(11 - elapsedTimeSinceDown)
+		} else {
+			revUpPlayer.start(0)
+		}
+		elapsedTimeSinceUp = ctx.now()
+		console.log("Pressing pedal down at: " + elapsedTimeSinceUp)
+	}
+})
+
+// On pedal up
+$("body").keyup(function(e) {
+	if (e.keyCode == 66) { // 66 = 'b'
+		// Stop the rev up sounds
+		elapsedTimeSinceUp = ctx.now() - elapsedTimeSinceUp
+		$("#stopwatch").text(elapsedTimeSinceUp)
+		console.log("Pedal pressed for: " + elapsedTimeSinceUp)
+		revUpPlayer.stop()
+
+		// And start rev down sounds at the correct time
+		elapsedTimeSinceDown = ctx.now()
+		revDownPlayer.start(0, 11 - elapsedTimeSinceUp)
+		console.log("Pedal let go at: " + elapsedTimeSinceDown)
+	}
+	delete keysdown[e.keyCode]
+})
